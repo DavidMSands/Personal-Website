ee78fa6df541fb13fe037c52e14096c4a0789e08
David <Davidmaxsands@gmail.com>
2022-01-02 17:51:09 -0700

    JS event for photo

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const hover = document.getElementById("helloBox"); 
+let hover = document.getElementById("professional");
+let rectangle = document.getElementById("Rectangle2");
+
+hover.addEventListener("mouseover", function(){
+    rectangle.style.left = "84.7%";
+    rectangle.style.bottom = "57.2%";
+    hover.style.filter = "contrast(90%)"
+},true);
+
+hover.addEventListener("mouseout", function(){
+    rectangle.style.left = "85%";
+    rectangle.style.bottom = "56.5%";
+    hover.style.filter = "contrast(100%)"
+},true);
 
 
-    document.addEventListener("mouseover", function (event) {
-          const leftNumbers = hover.style.left.replace("px", "");
-          const left = parseInt(leftNumbers, 10);
-          hover.style.left = `${left - 1}px`;
-        
-      });
+
+/*const leftNumbers = rectangle.style.left.replace("%", "");
+const left = parseInt(leftNumbers, 10);
+rectangle.style.left = `${left - 5}%`;
+console.log(hover);*/
