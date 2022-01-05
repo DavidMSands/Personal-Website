8bca8e5b96c481a389a763d49d3966339b2641b2
David <Davidmaxsands@gmail.com>
2022-01-05 15:32:55 -0700

    adding two new HTML files. Updating js file

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-let hover = document.getElementById("professional");
-let rectangle = document.getElementById("Rectangle2");
-let email = document.getElementById("email");
+let professional = document.getElementById("professional");
 
-hover.addEventListener("mouseover", function(){
-    rectangle.style.left = "84.7%";
-    rectangle.style.bottom = "57.2%";
-    hover.style.filter = "contrast(90%)"
+
+
+professional.addEventListener("mouseover", function(){
+    professional.style.filter = "contrast(90%)"
 },true);
 
-hover.addEventListener("mouseout", function(){
-    rectangle.style.left = "85%";
-    rectangle.style.bottom = "56.5%";
-    hover.style.filter = "contrast(100%)"
+professional.addEventListener("mouseout", function(){
+    professional.style.filter = "contrast(100%)"
 },true);
 
-email.addEventListener("mouseover", function(){
-    email.style.top = ("16%");
-},true);
 
-email.addEventListener("mouseout", function(){
-    email.style.top = ("17%");
-},true);
 
